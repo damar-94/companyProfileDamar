5025ce57d24174d4af39e560f85baec943096d06
Damar <andreasdamarjati1994@gmail.com>
2026-09-22 16:30:56 +0700

    fix: import icon

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,7 @@
-import { HugeiconsIcon } from '@hugeicons/react';
-import { Loading02Icon } from '@hugeicons/core-free-icons';
-
-function Loading(){
-    return <HugeiconsIcon icon={Loading02Icon} className="animate-spin"/>
+import { HugeiconsIcon } from "@hugeicons/react";
+import { Loading02Icon } from "@hugeicons/core-free-icons";
+function Loading() {
+  return <HugeiconsIcon icon={Loading02Icon} className="animate-spin" />;
 }
 
-export default Loading
+export default Loading;
